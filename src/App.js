0e4e70a1af47ba545d6609b38ef91c74be89e576
Rohan <rohansethi1999430@gmail.com>
2021-07-08 10:25:18 +0530

    Created Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hii!!!</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
